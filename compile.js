20ef60b2935bdbf60426594a25eb4f8c4aaac0f3
ishitarastogi <ishita3099@gmail.com>
2020-12-12 12:01:04 +0530

    compilation error

--- compile.js
+++ compile.js
@@ -1,15 +1,14 @@
 const path = require('path');
-const fs = require('fs'); // Built-in dependency for file streaming.
-const solc = require('solc'); // Our Solidity compiler
+const fs = require('fs'); 
+const solc = require('solc'); 
 const helloPath = path.resolve(__dirname,'contracts', 'HelloWorld.sol');
-const content = fs.readFileSync(helloPath, 'utf-8'); // Read the file...
+const content = fs.readFileSync(helloPath, 'utf-8'); 
 
-// Format the input for solc compiler:
 const input = {
   language: 'Solidity',
   sources: {
     'HelloWorld.sol' : {
-      content, // The imported content
+      content, 
     }
   },
   settings: {
@@ -21,6 +20,6 @@ const input = {
   }
 }; 
 
-const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-console.log(output);
+
+console.log(solc.compile(content,1).contracts[':HelloWorld']);
